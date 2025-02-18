e4b0b6568625e102f9433ef1e789e2acdb81ec41
Frogbit0811 <chienesf@gmail.com>
2025-02-18 21:58:16 +0700

    Fix-Script_PlayerMovement
    DI chuyển

--- Assets/Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Assets/Scripts/Player/PlayerMovement.cs
@@ -19,7 +19,50 @@ public class PlayerMovement : MonoBehaviour
     [Header("Health")]
     [SerializeField] int _maxHealth;
 
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>(); 
+    }
+
     #region States
+
+    public void Move(Vector3 direction)
+    {
+        transform.Translate(direction * _velo * Time.deltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.1f, _groundLayer);
+    }
+
+    void Update()
+    {
+        Vector3 direction = Vector3.zero; 
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction = Vector3.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            direction = Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.W) && IsGrounded())
+        {
+            _rigidbody.AddForce(Vector2.up * _JumpForce, ForceMode2D.Impulse);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _rigidbody.AddForce(Vector2.down * _JumpForce, ForceMode2D.Impulse);
+        }
+        if (direction != Vector3.zero)
+        {
+            Move(direction);
+        }
+    }
 
     #endregion
 }
